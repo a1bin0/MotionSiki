5521d821af1fcf8d69711533b40285399f6e5770
a1bin0 <si.h@aol.de>
2014-11-23 20:59:45 +0900

    最初のコミット

---
+++ Siki/MotionDetector.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenNI;
+
+namespace Siki
+{
+    class MotionDetector
+    {
+        #region
+
+        private DepthGenerator depth;
+        
+        #endregion
+
+        private Dictionary<int, List<Dictionary<SkeletonJoint, SkeletonJointPosition>>> histryData = new Dictionary<int,List<Dictionary<SkeletonJoint,SkeletonJointPosition>>>();
+
+        public event EventHandler LeftHandUpDetected;
+        public event EventHandler LeftHandDownDetected;
+        public event EventHandler LeftHandSwipeLeftDetected;
+        public event EventHandler LeftHandOverHeadDetected;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MotionDetector (DepthGenerator depth)
+        {
+            this.depth = depth;
+        }
+
+        public void DetectMotion(int userID, Dictionary<SkeletonJoint, SkeletonJointPosition> skeleton)
+        {
+            saveHistory(userID, skeleton);
+
+            if (DetectLeftHandSwipeUp(userID, skeleton))
+            {
+                this.LeftHandUpDetected(this, EventArgs.Empty);
+            }
+
+            if (DetectLeftHandDown(userID, skeleton))
+            {
+                this.LeftHandDownDetected(this, EventArgs.Empty);
+            }
+
+            if (DetectLeftHandSwipeLeft(userID, skeleton))
+            {
+                this.LeftHandSwipeLeftDetected(this, EventArgs.Empty);
+            }
+
+            if (DetectLeftHandOverHead(userID, skeleton))
+            {
+                this.LeftHandOverHeadDetected(this, EventArgs.Empty);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="skeleton"></param>
+        /// <returns></returns>
+        private void saveHistory(int userID, Dictionary<SkeletonJoint, SkeletonJointPosition> skeleton)
+        {
+            if (!this.histryData.ContainsKey(userID))
+            {
+                this.histryData.Add(userID, new List<Dictionary<SkeletonJoint, SkeletonJointPosition>>());
+            }
+
+            List<Dictionary<SkeletonJoint, SkeletonJointPosition>> positions = this.histryData[userID];
+
+            if (positions.Count > 4)
+            {
+                positions.RemoveAt(0);
+            }
+
+            positions.Add(skeleton);
+        }
+
+        /// <summary>
+        /// 左手が頭より横に30cm以上離れておらず、頭の上にあるか,
+        /// 　TODO両肩の座標とって比較しよう
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="skeleton"></param>
+        /// <returns></returns>
+        private bool DetectLeftHandOverHead(int userID, Dictionary<SkeletonJoint, SkeletonJointPosition> skeleton)
+        {
+            float distance = Math.Abs
+                ((depth.ConvertRealWorldToProjective(skeleton[SkeletonJoint.LeftHand].Position).X
+                - depth.ConvertRealWorldToProjective(skeleton[SkeletonJoint.Head].Position).X));
+
+            if (distance > 30.0) return false;
+
+            return (depth.ConvertRealWorldToProjective(skeleton[SkeletonJoint.LeftHand].Position).Y
+                < depth.ConvertRealWorldToProjective(skeleton[SkeletonJoint.Head].Position).Y);
+        }
+
+        /// <summary>
+        /// 左手が頭から約30cm以上離れた場所で左に移動したか
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="skeleton"></param>
+        /// <returns></returns>
+        private bool DetectLeftHandSwipeLeft(int userID, Dictionary<SkeletonJoint, SkeletonJointPosition> skeleton)
+        {
+            List<Dictionary<SkeletonJoint, SkeletonJointPosition>> positions = this.histryData[userID];
+
+            if (positions.Count < 5) return false;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Point3D oldData = depth.ConvertRealWorldToProjective(positions[i][SkeletonJoint.LeftHand].Position);
+                Point3D newData = depth.ConvertRealWorldToProjective(positions[i + 1][SkeletonJoint.LeftHand].Position);
+                if (oldData.X < newData.X) return false;
+            }
+
+            return positions.All(item => this.isFar(item));
+        }
+
+        /// <summary>
+        /// 左手が頭から約30cm以上離れた場所で下がっているか
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="skeleton"></param>
+        /// <returns></returns>
+        private bool DetectLeftHandDown(int userID, Dictionary<SkeletonJoint, SkeletonJointPosition> skeleton)
+        {
+            List<Dictionary<SkeletonJoint, SkeletonJointPosition>> positions = this.histryData[userID];
+
+            if (positions.Count < 5) return false;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Point3D oldData = depth.ConvertRealWorldToProjective(positions[i][SkeletonJoint.LeftHand].Position);
+                Point3D newData = depth.ConvertRealWorldToProjective(positions[i + 1][SkeletonJoint.LeftHand].Position);
+                if (oldData.Y > newData.Y) return false;
+            }
+
+            return positions.All(item => this.isFar(item));
+        }
+
+        /// <summary>
+        /// 左手が頭から約30cm以上離れた場所で上がっているか
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="skeleton"></param>
+        /// <returns></returns>
+        private bool DetectLeftHandSwipeUp(int userID, Dictionary<SkeletonJoint, SkeletonJointPosition> skeleton)
+        {
+            List<Dictionary<SkeletonJoint, SkeletonJointPosition>> positions = this.histryData[userID];
+
+            if (positions.Count < 5) return false;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Point3D oldData = depth.ConvertRealWorldToProjective(positions[i][SkeletonJoint.LeftHand].Position);
+                Point3D newData = depth.ConvertRealWorldToProjective(positions[i + 1][SkeletonJoint.LeftHand].Position);
+                if (oldData.Y < newData.Y) return false;
+            }
+
+            return positions.All(item => this.isFar(item));
+        }
+
+        /// <summary>
+        /// 頭と左手が約30cm以上横に離れているか
+        /// </summary>
+        /// <param name="skeleton"></param>
+        /// <returns></returns>
+        private bool isFar(Dictionary<SkeletonJoint, SkeletonJointPosition> skeleton)
+        {
+            float distance = 
+                (depth.ConvertRealWorldToProjective(skeleton[SkeletonJoint.Head].Position).X
+                - depth.ConvertRealWorldToProjective(skeleton[SkeletonJoint.LeftHand].Position).X);
+
+            return (distance > 30.0);
+        }
+    }
+}
---
+++ Siki/Siki.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using OpenNI;
+using System.Collections.Generic;
+
+namespace Siki
+{
+    //region書いて！
+
+    public partial class Siki : Form
+    {
+        private Context context;
+        private ScriptNode scriptNode;
+        private Thread readerThread;
+        private bool shouldRun;
+        private DepthGenerator depth;
+        private UserGenerator user;
+        private Dispatcher dispatcher = Dispatcher.CurrentDispatcher;
+        private MotionDetector motionDetector;
+
+        public Siki()
+        {
+            InitializeComponent();
+
+            this.context = Context.CreateFromXmlFile(@"./Config.xml", out this.scriptNode);
+            this.context.GlobalMirror = true;
+            this.depth = context.FindExistingNode(NodeType.Depth) as DepthGenerator;
+            this.user = new UserGenerator(context);
+
+            setupMotiondetector();
+
+            user.NewUser += this.user_NewUser;
+            user.LostUser += this.user_Lost;
+            user.SkeletonCapability.CalibrationStart += this.SkeletonCapability_CalibrationStart;
+            user.SkeletonCapability.CalibrationComplete += this.SkeletonCapability_CalibrationComplete;
+            user.SkeletonCapability.SetSkeletonProfile(SkeletonProfile.All);
+            context.StartGeneratingAll();
+
+            this.shouldRun = true;
+            readerThread = new Thread(new ThreadStart(ReaderThread));
+            this.readerThread.Start();
+        }
+
+        private void setupMotiondetector()
+        {
+            this.motionDetector = new MotionDetector(this.depth);
+
+            this.motionDetector.LeftHandDownDetected += this.leftHandDownDetected;
+            this.motionDetector.LeftHandUpDetected += this.leftHandUpDetected;
+            this.motionDetector.LeftHandSwipeLeftDetected += this.leftHandSwipeLeftDetected;
+            this.motionDetector.LeftHandOverHeadDetected += this.leftHandOverHeadDetected;
+        }
+
+        private void leftHandDownDetected(object sender, EventArgs e)
+        {
+            Console.WriteLine("down");
+        }
+
+        private void leftHandUpDetected(object sender, EventArgs e)
+        {
+            Console.WriteLine("up");
+        }
+
+        private void leftHandSwipeLeftDetected(object sender, EventArgs e)
+        {
+            Console.WriteLine("swipeLeft");
+        }
+
+        private void leftHandOverHeadDetected(object sender, EventArgs e)
+        {
+            Console.WriteLine("overHead");
+        }
+
+        void SkeletonCapability_CalibrationComplete(object sender, CalibrationProgressEventArgs e)
+        {
+            Console.WriteLine(String.Format("キャリブレーション終了: {0}", e.ID));
+            if (e.Status == CalibrationStatus.OK)
+            {
+                user.SkeletonCapability.StartTracking(e.ID);
+            }
+        }
+
+        void SkeletonCapability_CalibrationStart(object sender, CalibrationStartEventArgs e)
+        {
+            Console.WriteLine(String.Format("キャリブレーション開始: {0}", e.ID));
+
+        }
+
+        void user_NewUser(object sender, NewUserEventArgs e)
+        {
+            Console.WriteLine(String.Format("ユーザ検出: {0}", e.ID));
+            user.SkeletonCapability.RequestCalibration(e.ID, true);
+        }
+
+        private void user_Lost(object sender, UserLostEventArgs e)
+        {
+            Console.WriteLine(String.Format("ユーザ消失: {0}", e.ID));
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            this.shouldRun = false;
+            this.readerThread.Join();
+            base.OnClosing(e);
+        }
+
+        private void ReaderThread()
+        {
+            while (this.shouldRun)
+            {
+                this.context.WaitAndUpdateAll();
+                DepthMetaData depthMD = depth.GetMetaData();
+
+                this.dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                {
+                    var users = user.GetUsers();
+                    foreach (var u in users)
+                    {
+                        if (!user.SkeletonCapability.IsTracking(u)) continue;
+
+                        var pointDict = new Dictionary<SkeletonJoint, SkeletonJointPosition>();
+                        foreach (SkeletonJoint s in Enum.GetValues(typeof(SkeletonJoint)))
+                        {
+                            if (!user.SkeletonCapability.IsJointAvailable(s)) continue;
+                            pointDict.Add(s, user.SkeletonCapability.GetSkeletonJointPosition(u, s));
+                        }
+
+                        this.motionDetector.DetectMotion(u, pointDict);
+                    }
+                }));
+            }
+        }
+    }
+}
